f6328dcba18e7aa6c3e99e05188cb41722548fd6
anand-bits <114140559+anand-bits@users.noreply.github.com>
2024-02-22 22:51:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -53,47 +53,24 @@ class Solution
     { 
          // your code here 
          
-         int ans=0;
          int dp[][]= new int[n+1][W+1];
+       
          
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
          
-         
-        ans= solve(W,wt,val,n,dp);
-         return ans;
          
     } 
-    
-   static  int solve(int W,int wt[],int val[],int n,int dp[][])
-    {
-        if(n==0 || W==0)
-        {
-            return 0;
-        }
-        
-        if(dp[n][W]!=0)
-        {
-            return dp[n][W];
-            
-        }
-        
-         int include=0;
-          int exclude=0;
-      if(wt[n-1]<=W)
-        {
-             include=val[n-1]+ solve(W-wt[n-1],wt,val,n-1,dp);
-             exclude=solve(W,wt,val,n-1,dp);
-             
-            return  dp[n][W]=Math.max(include ,exclude);
-            
-        }
-        
-       
-        return dp[n][W]=solve(W,wt,val,n-1,dp);
-        
-        
-        
-        
-    }
 }
 
 
