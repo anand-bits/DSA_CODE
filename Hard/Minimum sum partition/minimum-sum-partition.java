70d302130f46d077c047958f35ebcd7a84ba53f3
anand-bits <114140559+anand-bits@users.noreply.github.com>
2024-02-28 01:01:27 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Minimum sum partition/minimum-sum-partition.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int A[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                        A[i] = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minDifference(A,n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+
+	public int minDifference(int arr[], int n) 
+	{ 
+	    // Your code goes here
+	    int totalSum = Arrays.stream(arr).sum();
+        int maxPossibleSum = totalSum / 2;
+
+        boolean[][] dp = new boolean[n + 1][maxPossibleSum + 1];
+
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= maxPossibleSum; j++) {
+                if (arr[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        int j = maxPossibleSum;
+        while (j >= 0 && !dp[n][j])
+            j--;
+
+        int minDiff = totalSum - 2 * j;
+        return minDiff;
+    }
+        
+	    
+	  
+	
+
+}
